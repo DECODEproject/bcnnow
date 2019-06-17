f0dd748389573d2f65cfa968c29ba04a718aa79b
Rohit Kumar <45081607+rohit-nlp@users.noreply.github.com>
2019-06-17 16:15:38 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -42,8 +42,8 @@ class Config:
                 "client_password": "V2CQt67jOXTpeV4BrDMumQOcka1HEpQmDWp72l1mnutz52j8"
             },
             "encryption":{
-                    "public": "BE5k24WolPVFB0OLjYZk0/VMSewVr4MARy8YfU8vGEaNIJDVqK3BUd/NVi7wctITK2+3AnwWNFwCV/oPsvhdBek=",
-                    "private": "CPzY3PvJXXwl9JVWKyLhpo36xbD3729XBZV3XoTVig8="
+                    "public": "***",
+                    "private": "***"
 
             }
         }
