797b435cfc262edcf4ae83f5b92d592e71b14d56
elaragon <elaragon@gmail.com>
2019-06-25 16:47:40 +0200

    fix heatmaps & relative dates

--- apps/frontend/js/dashboard/config.js
+++ apps/frontend/js/dashboard/config.js
@@ -4,5 +4,7 @@
 //var url_api = 'http://93ff8837.ngrok.io/api/v0/'; // Get the endpoint url of BarcelonaNow API
 //var url_api = 'http://172.20.120.110:9530/api/v0/'; // Get the endpoint url of BarcelonaNow API
 //var url_root = 'http://172.20.120.110:9530/';
-var url_api = 'http://127.0.0.1:8080/api/v0/'; // Get the endpoint url of BarcelonaNow API
-var url_root = 'http://127.0.0.1:8080/';
+//var url_api = 'http://127.0.0.1:8080/api/v0/'; // Get the endpoint url of BarcelonaNow API
+//var url_root = 'http://127.0.0.1:8080/';
+var url_api = 'http://bcnnow.decodeproject.eu:9530/api/v0/'; // Get the endpoint url of BarcelonaNow API
+var url_root = 'http://bcnnow.decodeproject.eu:9530/';
